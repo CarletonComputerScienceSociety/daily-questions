f3bcb6816c3485d14c2c242adb178190aa1c6df1
Bailey D'Amour <bailey@damour.xyz>
2019-07-05 09:09:48 -0400

    Improve worst case complexity for July 5 solution

--- 2019/07/05/Solutions/DamourYouKnow/solution.py
+++ 2019/07/05/Solutions/DamourYouKnow/solution.py
@@ -1,11 +1,12 @@
 def longest_sequence(arr):
     arr_set = set(arr)
     counts = []
-    for num in arr_set:
+    for num in arr:
         higher, lower = 0, 0
         while num + higher + 1 in arr_set: higher += 1
         while num - lower - 1 in arr_set: lower += 1
         counts.append(higher + lower + 1)
+        arr_set -= {n for n in range(num - lower, num + higher + 1)}
     return max(counts)
 
 
