fc16dfebe426d8f2c515cc2dc0ddede3ad6cc477
Bailey D'Amour <bailey@damour.xyz>
2019-07-11 09:34:56 -0400

    Add not "cheating" solution for July 11

--- 2019/07/11/Solutions/DamourYouKnow/solution.py
+++ 2019/07/11/Solutions/DamourYouKnow/solution.py
@@ -1,6 +1,13 @@
-import itertools
+def permutations(elems):
+    if len(elems) == 0:
+        return []
+    if len(elems) == 1:
+        return [elems]
 
-def permutations(elements):
-    return list(itertools.permutations(elements))
+    perms = []
+    for e in range(len(elems)):
+        perms += [[elems[e]] + p for p in permutations(elems[:e] + elems[e + 1:])]
+    return perms
 
-print(permutations({1, 2, 3}))
+
+print(permutations([1,2,3]))
