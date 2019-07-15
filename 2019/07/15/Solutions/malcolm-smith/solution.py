9f453548282d808da2a37cab4405692d9e072662
malcolm-smith <malcolm.henry.smith@gmail.com>
2019-07-15 11:46:46 -0400

    solution in python

---
+++ 2019/07/15/Solutions/malcolm-smith/solution.py
@@ -0,0 +1,9 @@
+def jewelCount(j, s):
+    count = 0
+    for i in range(len(s)):
+        if s[i] in j:
+            count+=1
+    return count
+
+print(jewelCount("aA", "aAAbbbb"))
+print(jewelCount("z", "ZZ"))
