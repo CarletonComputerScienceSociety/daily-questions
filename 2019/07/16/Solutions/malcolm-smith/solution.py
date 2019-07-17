11272535b5c962ade1ea1a8fc12d2221834d12b4
malcolm-smith <malcolm.henry.smith@gmail.com>
2019-07-16 21:16:43 -0400

    edit

--- 2019/07/16/Solutions/malcolm-smith/solution.py
+++ 2019/07/16/Solutions/malcolm-smith/solution.py
@@ -1,5 +1,4 @@
 def separateEvenOdd(a):
-    arr = [i for i in a if ((i % 2) == 0)] + [i for i in a if ((i % 2) != 0)]
-    return arr
+    return [i for i in a if ((i % 2) == 0)] + [i for i in a if ((i % 2) != 0)]
 
 print(separateEvenOdd([ 3, 1, 2, 4 ]))
